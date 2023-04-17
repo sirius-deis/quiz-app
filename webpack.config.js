adada75dd9abb39e6f7ed222675b36316d9bbcfb
Sirius Deis <falebsy@gmail.com>
2023-04-17 07:21:44 +0200

    add webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
     mode: "development",
@@ -8,5 +10,43 @@ module.exports = {
         filename: "bundle.js",
         path: path.resolve(__dirname, "dist"),
         clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /m?\.js/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"],
+                    },
+                },
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "src", "index.html"),
+            filename: "index.html",
+            title: "Quiz app",
+            inject: "body",
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, "src", "assets", "icons"),
+                    to: "public/icons",
+                },
+            ],
+        }),
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "dist"),
+        },
+        hot: true,
+        compress: true,
+        port: 3001,
     },
 };
