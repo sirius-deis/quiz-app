9a6ad6979e904ab1febcceafb4f38cda1a39b6df
Sirius Deis <falebsy@gmail.com>
2023-04-23 11:32:07 +0200

    add functions for replacing state on clicking app button and returning to previos section on clicking back and forward buttons

--- src/index.js
+++ src/index.js
@@ -1,6 +1,68 @@
 import "./categories";
 
-const url = new URL(location);
-// history.pushState({}, "", `${url}categories`);
+const startSection = document.querySelector(".start");
+const starterBtnEl = startSection.querySelector(".btn");
+const infoSection = document.querySelector(".info");
 
-console.log(window.location);
+const url = new URL(location);
+let current = "";
+let currentSection;
+
+window.addEventListener("DOMContentLoaded", (e) => {
+    const name = retrieveNameFromStorage();
+
+    if (url.pathname !== "/" && !name) {
+        currentSection = startSection;
+        replace();
+    }
+    starterBtnEl &&
+        starterBtnEl.addEventListener("click", () => {
+            currentSection = infoSection;
+            move("info");
+            showSection(infoSection);
+            hideSection(startSection);
+        });
+});
+
+const replace = (path = "") => {
+    history.replaceState(null, "", `${url.origin}/${path}`);
+};
+
+const move = (path) => {
+    current = `${url.origin}/${path}`;
+    history.pushState(null, "", current);
+};
+
+const showSection = (section) => {
+    section.classList.remove("hidden");
+
+    setTimeout(() => {
+        section.classList.add(`visible`);
+    }, 10);
+};
+
+const hideSection = (section) => {
+    section.classList.remove("visible");
+    setTimeout(() => {
+        section.classList.add("hidden");
+    }, 300);
+};
+
+const retrieveNameFromStorage = () => {
+    return localStorage.getItem("name");
+};
+
+const putNameToStorage = (name) => {
+    localStorage.setItem("name", name);
+};
+
+window.addEventListener("popstate", (e) => {
+    const section = new URL(e.target.location).pathname.slice(1);
+    if (section === current) {
+        return;
+    }
+    const sectionToReplace = document.querySelector(`.${section || "start"}`);
+    showSection(sectionToReplace);
+    hideSection(currentSection);
+    currentSection = sectionToReplace;
+});
